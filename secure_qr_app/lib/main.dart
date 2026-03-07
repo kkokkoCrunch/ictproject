257cdeaea6b4b5ad7a27cdc972cf62c6ebdd759c
kkokko <96097871+kkokkoCrunch@users.noreply.github.com>
2026-03-07 15:14:14 +0800

    Update main.dart

--- secure_qr_app/lib/main.dart
+++ secure_qr_app/lib/main.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 import 'login_page.dart';
+import 'package:uni_links/uni_links.dart';
 
 void main() {
   runApp(const SecureQrApp());
 }
 
-class SecureQrApp extends StatelessWidget {
+class SecureQrApp extends StatefulWidget {
   const SecureQrApp({super.key});
+
+  @override
+  State<SecureQrApp> createState() => _SecureQrAppState();
+}
+
+class _SecureQrAppState extends State<SecureQrApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    initDeepLink();
+  }
+
+  void initDeepLink() async {
+    final uri = await getInitialUri();
+
+    if (uri != null) {
+      final token = uri.queryParameters['token'];
+
+      if (token != null) {
+        print("Token received: $token");
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
